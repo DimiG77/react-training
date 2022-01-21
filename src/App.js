0d9339d11c696c849a33b11ef213ada795e23a33
DimiG77 <d.gazetis@outlook.com>
2022-01-21 13:42:06 +0200

    Revert "Dynamic Values/props/Css/Components"
    This reverts commit 96ae1541755d1022a839f8b6eb2f08839efdff8a.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
-
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2022, 0, 23),
-    },
-
-    {
-      id: "e2",
-      title: "Gaming Monitor",
-      amount: 250.23,
-      date: new Date(2022, 1, 23),
-    },
-
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 100.12,
-      date: new Date(2022, 1, 22),
-    },
-
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 402.32,
-      date: new Date(2022, 5, 23),
-    },
-  ];
-
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
     </div>
   );
 }
